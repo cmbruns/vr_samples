a4775ef81e97f2c3c5b4a1ceda2e444f6d0a35d7
Christopher Bruns <cmbruns@rotatingpenguin.com>
2017-01-29 16:29:21 -0500

    Render pink world in photosphere1.py stub.

--- src/parallax_corrected_photospheres/pyopenvr/photosphere1.py
+++ src/parallax_corrected_photospheres/pyopenvr/photosphere1.py
@@ -1,5 +1,31 @@
 #!/bin/env python
 
+import sys
+
+from OpenGL.GL import * # @UnusedWildImport # this comment squelches IDE warnings
+
 import openvr
+from openvr.glframework.glfw_app import GlfwApp
+from openvr.gl_renderer import OpenVrGlRenderer
+
+class SphericalPanorama(object):
+    def __init__(self):
+        pass
+
+    def init_gl(self):
+        pass
+
+    def display_gl(self, modelview, projection):
+        glClearColor(1, 0.9, 0.9, 1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    def dispose_gl(self):
+        pass
 
 print ("Hello")
+
+actor = SphericalPanorama()
+renderer = OpenVrGlRenderer(actor)
+with GlfwApp(renderer, "glfw OpenVR color cube") as glfwApp:
+    glfwApp.run_loop()
+
