4e34b6cafc8bb9a8ca2e31b90fc8c92e5c400b04
Christopher Bruns <cmbruns@rotatingpenguin.com>
2017-03-29 17:11:40 -0400

    Create cubemap image, but it has a lod seam in the back.

--- src/python/vrprim/photosphere/conv.py
+++ src/python/vrprim/photosphere/conv.py
@@ -20,17 +20,11 @@ class Converter(object):
         GL.glUniform1i(equirect_loc, 0)
         GL.glDrawArrays(GL.GL_TRIANGLE_STRIP, 0, 4)
 
-    def cube_from_equirect(self, arr0):
+    def cube_from_equirect(self, arr):
         """
         Use OpenGL to efficiently warp an equirectangular image into
         a single cubemap image
         """
-        # Convert to 16-bit uint
-        fmax = numpy.amax(arr0)
-        fmin = numpy.amin(arr0)
-        print(fmin, fmax)
-        arr = arr0 * 65535.99/fmax
-        arr = arr.astype('uint16')
         # Set up glfw
         eh = arr.shape[0]
         ew = arr.shape[1]
@@ -63,7 +57,7 @@ class Converter(object):
             print("Framebuffer OK")
         # Create shader program
         vtx = shaders.compileShader("""#version 450
-            #line 54
+            #line 61
 
             out vec2 tex_coord;
 
@@ -76,21 +70,87 @@ class Converter(object):
             void main() {
                 vec4 c = SCREEN_QUAD[gl_VertexID]; // corner location
                 gl_Position = c;
-                tex_coord = 0.5 * (c.xy + vec2(1, 1));
+                tex_coord = 0.5 * (c.xy + vec2(1));
             }
             """, GL.GL_VERTEX_SHADER)
         frg = shaders.compileShader("""#version 450
-            #line 70
+            #line 78
 
             layout(binding=0) uniform sampler2D equirect;
 
             in vec2 tex_coord;
             out vec4 frag_color;
 
+            const float PI = 3.14159265359;
+
+            vec3 xyz_from_equirect(in vec2 eq) {
+                vec2 c = 2*eq - vec2(1); // centered
+                float lon = PI * c.x;
+                float lat = -0.5 * PI * c.y;
+                float s = cos(lat);
+                return vec3(s*sin(lon), sin(lat), -s*cos(lon));
+            }
+
+            vec2 equirect_from_xyz(in vec3 xyz) {
+                float r = length(xyz.xz);
+                float lat = atan(xyz.y, r);
+                float lon = atan(xyz.x, -xyz.z);
+                return 0.5 * (vec2(lon / PI, -2.0 * lat / PI) + vec2(1));
+            }
+
+            vec3 xyz_from_cube(in vec2 cube) {
+                if (cube.y > 2.0/3.0) { // lower strip
+                    if (cube.x < 1.0/4.0) {
+                        discard;
+                    }
+                    else if (cube.x > 2.0/4.0) {
+                        discard;
+                    }
+                    else {
+                        vec2 xy = (cube - vec2(3.0/8.0, 5.0/6.0)) * vec2(8, -6);
+                        return normalize(vec3(xy.x, -1, -xy.y)); // bottom
+                    }
+                }
+                else if (cube.y < 1.0/3.0) { // upper strip
+                    if (cube.x < 1.0/4.0) {
+                        discard;
+                    }
+                    else if (cube.x > 2.0/4.0) {
+                        discard;
+                    }
+                    else { // top
+                        vec2 xy = (cube - vec2(3.0/8.0, 1.0/6.0)) * vec2(8, -6);
+                        return normalize(vec3(xy.x, 1, xy.y));
+                    }
+                }
+                else { // central strip
+                    if (cube.x < 0.25) {
+                        vec2 xy = (cube - vec2(1.0/8.0, 0.5)) * vec2(8, -6);
+                        return normalize(vec3(-1, xy.y, -xy.x)); // left
+                    }
+                    else if (cube.x < 0.50) { // front
+                        vec2 xy = (cube - vec2(3.0/8.0, 0.5)) * vec2(8, -6);
+                        return normalize(vec3(xy.x, xy.y, -1));
+                    }
+                    else if (cube.x < 0.75) { // right
+                        vec2 xy = (cube - vec2(5.0/8.0, 0.5)) * vec2(8, -6);
+                        return normalize(vec3(1, xy.y, xy.x));
+                    }
+                    else { // back
+                        vec2 xy = (cube - vec2(7.0/8.0, 0.5)) * vec2(8, -6);
+                        return normalize(vec3(-xy.x, xy.y, 1));
+                    }
+                }
+            }
+
             void main() {
-                // frag_color = vec4(0, 0, 1, 1);
-                frag_color = texture(equirect, tex_coord);
+                vec3 xyz = xyz_from_cube(tex_coord);
+                vec2 eq = equirect_from_xyz(xyz);
+                // frag_color = vec4(eq, 0.5, 1);
+                // frag_color = vec4(xyz, 1);
+                frag_color = 50 * texture(equirect, eq);
                 // frag_color = vec4(tex_coord, 1, 1);
+                // frag_color = vec4(xyz_from_equirect(tex_coord), 1);
             }
             """, GL.GL_FRAGMENT_SHADER)
         self.shader = shaders.compileProgram(vtx, frg)
@@ -110,7 +170,7 @@ class Converter(object):
         GL.glDisable(GL.GL_BLEND)
         GL.glDisable(GL.GL_DEPTH_TEST)
         GL.glViewport(0, 0, cw, ch)
-        GL.glClearColor(0, 1, 0, 1)
+        GL.glClearColor(0.5, 0.5, 0.5, 0.0)
         # Render the image
         bToScreen = False
         if bToScreen:
@@ -150,7 +210,26 @@ def main():
     tif = TIFF.open('1w180.9.tiff', 'r')
     arr = tif.read_image()
     tif.close()
-
+    fmin = numpy.amin(arr[numpy.nonzero(arr)])
+    # Clip data to percentile range with dynamic range below 65535
+    pct_low = 0
+    pct_high = 100
+    val_low, val_high = numpy.percentile(arr[numpy.nonzero(arr)], [pct_low, pct_high])
+    dynamic_range = val_high / val_low
+    eps = 1e-2
+    while dynamic_range > 65535:
+        pct_low = eps
+        pct_high = 100.0 - eps
+        val_low, val_high = numpy.percentile(arr[numpy.nonzero(arr)], [pct_low, pct_high])
+        dynamic_range = val_high / val_low
+        print(pct_low, pct_high, val_low, val_high, dynamic_range)
+        eps *= 1.5
+    arr *= 65535.0 / val_high
+    arr[arr>65535] = 65535
+    arr[arr<0] = 0
+    print(numpy.histogram(arr))
+    arr = arr.astype('uint16')
+    #
     cube = Converter().cube_from_equirect(arr)
     print(cube.shape)
     # This is the slow part
@@ -163,6 +242,7 @@ def main():
     # print(arr)
     print(numpy.amin(arr))
     fmax = numpy.amax(arr)
+    print(fmax)
     # Convert to 16-bit uint
     arr2 = arr * 65535.99/fmax
     arr2 = arr2.astype('uint16')
